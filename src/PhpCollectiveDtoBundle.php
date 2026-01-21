031d08a2231dd3c2ec25dbb8770f50ce8fc962c8
mscherer <dereuromark@web.de>
2026-01-21 17:18:35 +0100

    Add DoctrineCollectionAdapter for collection adapter pattern
    - Add src/Collection/DoctrineCollectionAdapter.php
    - Register adapter in PhpCollectiveDtoBundle boot()
    
    This enables the collection adapter pattern from php-collective/dto#55
    for Doctrine's ArrayCollection.
    
    Depends on: php-collective/dto#55

--- src/PhpCollectiveDtoBundle.php
+++ src/PhpCollectiveDtoBundle.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace PhpCollective\SymfonyDto;
 
 use Doctrine\Common\Collections\ArrayCollection;
+use PhpCollective\Dto\Collection\CollectionAdapterRegistry;
 use PhpCollective\Dto\Dto\Dto;
+use PhpCollective\SymfonyDto\Collection\DoctrineCollectionAdapter;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class PhpCollectiveDtoBundle extends Bundle
@@ -14,5 +16,8 @@ class PhpCollectiveDtoBundle extends Bundle
     {
         // Auto-configure Doctrine collections for DTO collection fields
         Dto::setCollectionFactory(fn (array $items) => new ArrayCollection($items));
+
+        // Register Doctrine collection adapter for proper template generation
+        CollectionAdapterRegistry::register(new DoctrineCollectionAdapter());
     }
 }
